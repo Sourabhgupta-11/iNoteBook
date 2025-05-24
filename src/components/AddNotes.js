50a2b3953dd26ade8b81608215796ca5a52f432b
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-05-24 17:39:37 +0530

    Add Notes(main notes file),NoteItem(each note structure),AddNotes(for adding notes) file

---
+++ src/components/AddNotes.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from 'react'
+import noteContext from '../context/notes/noteContext'
+
+const AddNotes = () => {
+const context=useContext(noteContext)
+const {addNote}=context
+
+const [note,setNote]=useState({title:"",description:"",tag:""})
+const handleClick=(e)=>{
+    e.preventDefault();
+    addNote(note.title,note.description,note.tag)
+    setNote({title:"",description:"",tag:""})
+}
+
+const onChange=(e)=>{
+    setNote({...note, [e.target.name]:e.target.value})
+}
+  return (
+    <div className="container my-3">
+      <h2>Add Your Notes</h2>
+      <form> 
+    <div className="mb-3 my-3">
+      <label htmlFor="title" className="form-label">Title</label>
+      <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" value={note.title} onChange={onChange}/>
+    </div>
+    <div className="mb-3">
+      <label htmlFor="desc" className="form-label">Description</label>
+      <input type="text" className="form-control" id="description" name="description" aria-describedby="emailHelp" value={note.description} onChange={onChange}/>
+    </div>
+    <div className="mb-3">
+      <label htmlFor="desc" className="form-label">Tag</label>
+      <input type="text" className="form-control" id="tag" name="tag" aria-describedby="emailHelp" value={note.tag} onChange={onChange}/>
+    </div>
+    <button type="submit" className="btn btn-primary" onClick={handleClick}>Submit</button>
+    </form>
+    </div>
+  )
+}
+
+export default AddNotes
