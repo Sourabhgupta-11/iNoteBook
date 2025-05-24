2840c66d058f78fffc812488f35bb57ac5e0f194
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-05-24 17:13:41 +0530

    Clean up default React boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import NoteState from './context/notes/noteState';
+import SignUp from './components/SignUp';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NoteState>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signUp" element={<SignUp/>}/>
+        </Routes>
+      </Router>
+    </NoteState>
   );
 }
 
