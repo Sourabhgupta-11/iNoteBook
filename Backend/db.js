c4cd5e74c8a657aa70ec473cfcfcf9a10031ef8b
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-05-24 17:22:17 +0530

    Add MongoDB connection setup in db.js

---
+++ Backend/db.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+
+const mongoURL ="mongodb://127.0.0.1:27017/iNotebook" 
+
+mongoose.connect(mongoURL)
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
+db.on('error', (err) => {
+  console.log('Error opening MongoDB connection:', err);
+});
+
+
+module.exports=db;
