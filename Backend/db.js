55b736889127c010cb0a28e0482bd546fdda46ef
Sourabhgupta-11 <sourabhgupta.1331@gmail.com>
2025-06-06 22:18:42 +0530

    Organize structure of our project and connect to MongoDB Atlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,9 @@
 const mongoose=require('mongoose');
+require('dotenv').config();
 
-const mongoURL ="mongodb://127.0.0.1:27017/iNotebook" 
+//const mongoURL ="mongodb://127.0.0.1:27017/iNotebook" #Local mongodb url
+
+const mongoURL=process.env.MONGODB_URL
 
 mongoose.connect(mongoURL)
 
